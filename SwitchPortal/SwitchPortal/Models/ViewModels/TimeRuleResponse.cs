f1b99465a87c7c2ab66131cf17faa874e5ec3cde
Peter Akintaro <peter.akintaro@coralpay.com>
2024-03-09 14:32:15 +0000

    TimeRule actions

---
+++ SwitchPortal/SwitchPortal/Models/ViewModels/TimeRuleResponse.cs
@@ -0,0 +1,10 @@
+﻿using SwitchPortal.Models.ViewModels.Rules;
+
+namespace SwitchPortal.Models.ViewModels
+{
+    public class TimeRuleResponse
+    {
+        public ResponseHeader ResponseHeader { get; set; }
+        public List<TimeDetails> TimeDetails { get; set; }
+    }
+}
