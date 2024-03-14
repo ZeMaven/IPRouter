59e0ce1ed01d2ad7be8d5d6f9d5e877441bc43de
Peter Akintaro <peter.akintaro@coralpay.com>
2024-03-14 08:06:17 +0000

    NIPoutward

--- NipOutwardProxy/Actions/NipOutward.cs
+++ NipOutwardProxy/Actions/NipOutward.cs
@@ -1,5 +1,8 @@
 ﻿using Momo.Common.Actions;
 using Momo.Common.Models;
+using NipOutwardProxy.Models.Nibss;
+using System.Text.Json;
+using NipService;
 
 namespace NipOutwardProxy.Actions
 {
@@ -18,30 +21,139 @@ namespace NipOutwardProxy.Actions
         private readonly ICommonUtilities Utilities;
         private readonly IHttpService HttpService;
         private readonly IPgp Pgp;
+        private readonly IXmlConverter XmlConverter;
+        private readonly NibssPortClient Nip;
 
-        public NipOutward(IConfiguration config, ILog log, ICommonUtilities utilities, IHttpService httpService, IPgp pgp)
+        public NipOutward(IConfiguration config, ILog log, ICommonUtilities utilities, IHttpService httpService, IPgp pgp, IXmlConverter xmlConverter)
         {
             Log = log;
             Config = config;
             Utilities = utilities;
             HttpService = httpService;
             Pgp = pgp;
+            XmlConverter = xmlConverter;
+            Nip = new NibssPortClient();
         }
-
-
 
         public async Task<NameEnquiryPxResponse> NameEnquiry(NameEnquiryPxRequest Request)
         {
-            return new NameEnquiryPxResponse { };
+            try
+            {
+                string SessionId = string.Empty;
+                string SourceBank = string.Empty;
+                var TranId = Utilities.CreateTransactionId();
+                SourceBank = Config.GetSection("SourceBank").Value;
+                SessionId = $"{SourceBank}{TranId}";
 
+                Log.Write("NibssOutward.NameEnquiry", $"Request from Router: {JsonSerializer.Serialize(Request)}");
+                var NibssRequest = new NESingleRequest
+                {
+                    AccountNumber = Request.AccountId,
+                    ChannelCode = Request.ChannelCode,
+                    DestinationInstitutionCode = Request.DestinationBankCode,
+                    SessionID = SessionId
+                };
+                var XmlRequest = XmlConverter.Serialize(NibssRequest);
+                Log.Write("NibssOutward.NameEnquiry", $"Request to Nibss xml: {XmlRequest}");
+                var EncRequest = Pgp.Ecryption(XmlRequest);
+                Log.Write("NibssOutward.NameEnquiry", $"Request to Nibss enc: {EncRequest}");
+                var NipResp = await Nip.nameenquirysingleitemAsync(EncRequest);
+                var NibssResponseEnc = NipResp.nameenquirysingleitemResponse1;
+
+                Log.Write("NibssOutward.NameEnquiry", $"Response from Nibss enc: {EncRequest}");
+
+                var NibssResponseXml = Pgp.Decryption(NibssResponseEnc);
+                Log.Write("NibssOutward.NameEnquiry", $"Response from Nibss Xml: {NibssResponseXml}");
+
+                NESingleResponse ResponseObj = (NESingleResponse)XmlConverter.DeSerialize(NibssResponseXml, new NESingleResponse());
+
+                var RouterResponse = new NameEnquiryPxResponse
+                {
+                    AccountName = ResponseObj.AccountName,
+                    AccountNumber = ResponseObj.AccountNumber,
+                    Bvn = ResponseObj.BankVerificationNumber,
+                    ChannelCode = ResponseObj.ChannelCode,
+                    DestinationBankCode = ResponseObj.DestinationInstitutionCode,
+                    KycLevel = ResponseObj.KYCLevel,
+                    ResponseCode = ResponseObj.ResponseCode,
+                    ResponseMessage = ResponseObj.ResponseCode == "00" ? "Successful" : null,
+                    SessionId = ResponseObj.SessionID,
+                    TransactionId = Request.TransactionId,
+                    SourceBankCode = Request.SourceBankCode,
+                };
+
+                Log.Write("NibssOutward.NameEnquiry", $"Response to Router: {JsonSerializer.Serialize(RouterResponse)}");
+                return RouterResponse;
+            }
+            catch (Exception Ex)
+            {
+                Log.Write("NibssOutward.NameEnquiry", $"Err: {Ex.Message}");
+                return new NameEnquiryPxResponse
+                {
+                    ResponseCode = "01",
+                    ResponseMessage = "Failed",
+                    TransactionId = Request.TransactionId,
+                    SourceBankCode = Request.SourceBankCode,
+                };
+            }
         }
 
 
         public async Task<TranQueryPxResponse> TransactionQuery(TranQueryPxRequest Request)
         {
-            return new TranQueryPxResponse { };
+            try
+            {
+                string SessionId = string.Empty;
+                string SourceBank = string.Empty;
+                var TranId = Utilities.CreateTransactionId();
+                SourceBank = Config.GetSection("SourceBank").Value;
+                SessionId = $"{SourceBank}{TranId}";
 
+                Log.Write("NibssOutward.TranQuery", $"Request from Router: {JsonSerializer.Serialize(Request)}");
+                var NibssRequest = new TSQuerySingleRequest
+                {
+                    SourceInstitutionCode = SourceBank,
+                    ChannelCode = 0,
+                    SessionID = SessionId
+                };
+                var XmlRequest = XmlConverter.Serialize(NibssRequest);
+                Log.Write("NibssOutward.TranQuery", $"Request to Nibss xml: {XmlRequest}");
+                var EncRequest = Pgp.Ecryption(XmlRequest);
+                Log.Write("NibssOutward.TranQuery", $"Request to Nibss enc: {EncRequest}");
+                //incorrect
+                var NipResp = await Nip.balanceenquiryAsync(EncRequest);
+                var NibssResponseEnc = NipResp.balanceenquiryResponse1;
+                //
+                Log.Write("NibssOutward.TranQuery", $"Response from Nibss enc: {EncRequest}");
 
+                var NibssResponseXml = Pgp.Decryption(NibssResponseEnc);
+                Log.Write("NibssOutward.TranQuery", $"Response from Nibss Xml: {NibssResponseXml}");
+
+                TSQuerySingleResponse ResponseObj = (TSQuerySingleResponse)XmlConverter.DeSerialize(NibssResponseXml, new TSQuerySingleResponse());
+
+                var RouterResponse = new TranQueryPxResponse
+                {
+                    ChannelCode = ResponseObj.ChannelCode,
+                    ResponseCode = ResponseObj.ResponseCode,
+                    ResponseMessage = ResponseObj.ResponseCode == "00" ? "Successful" : null,
+                    SessionId = ResponseObj.SessionID,
+                    TransactionId = Request.TransactionId,
+                    SourceBankCode = SourceBank
+                };
+
+                Log.Write("NibssOutward.TranQuery", $"Response to Router: {JsonSerializer.Serialize(RouterResponse)}");
+                return RouterResponse;
+            }
+            catch (Exception Ex)
+            {
+                Log.Write("NibssOutward.TranQuery", $"Err: {Ex.Message}");
+                return new TranQueryPxResponse
+                {
+                    ResponseCode = "01",
+                    ResponseMessage = "Failed",
+                    TransactionId = Request.TransactionId,
+                };
+            }
         }
 
 
@@ -49,9 +161,85 @@ namespace NipOutwardProxy.Actions
 
         public async Task<FundTransferPxResponse> Transfer(FundTransferPxRequest Request)
         {
+            try
+            {
+                string SessionId = string.Empty;
+                string SourceBank = string.Empty;
+                var TranId = Utilities.CreateTransactionId();
+                SourceBank = Config.GetSection("SourceBank").Value;
+                SessionId = $"{SourceBank}{TranId}";
 
-            return new FundTransferPxResponse { };
+                Log.Write("NibssOutward.Transfer", $"Request from Router: {JsonSerializer.Serialize(Request)}");
+                var NibssRequest = new FTSingleCreditRequest
+                {
+                    Amount = Request.Amount,
+                    BeneficiaryAccountName = Request.BenefAccountName,
+                    BeneficiaryAccountNumber = Request.BenefAccountNumber,
+                    BeneficiaryBankVerificationNumber = Request.BenefBvn,
+                    BeneficiaryKYCLevel = Request.BenefKycLevel,
+                    ChannelCode = Request.ChannelCode,
+                    DestinationInstitutionCode = Request.DestinationBankCode,
+                    MandateReferenceNumber = "",
+                    NameEnquiryRef = Request.NameEnquiryRef,
+                    Narration = Request.Narration,
+                    OriginatorAccountName = Request.SourceAccountName,
+                    OriginatorAccountNumber = Request.SourceAccountNumber,
+                    OriginatorBankVerificationNumber = "",
+                    OriginatorKYCLevel = "",
+                    PaymentReference = Request.TransactionId,
+                    SessionID = SessionId,
+                    TransactionFee = 0,
+                    TransactionLocation = ""
+                };
+                var XmlRequest = XmlConverter.Serialize(NibssRequest);
+                Log.Write("NibssOutward.Transfer", $"Request to Nibss xml: {XmlRequest}");
+                var EncRequest = Pgp.Ecryption(XmlRequest);
+                Log.Write("NibssOutward.Transfer", $"Request to Nibss enc: {EncRequest}");
+                var NipResp = await Nip.fundtransfersingleitem_dcAsync(EncRequest);
+                var NibssResponseEnc = NipResp.fundtransfersingleitem_dcResponse1;
 
+                Log.Write("NibssOutward.Transfer", $"Response from Nibss enc: {EncRequest}");
+
+                var NibssResponseXml = Pgp.Decryption(NibssResponseEnc);
+                Log.Write("NibssOutward.Transfer", $"Response from Nibss Xml: {NibssResponseXml}");
+
+                FTSingleCreditResponse ResponseObj = (FTSingleCreditResponse)XmlConverter.DeSerialize(NibssResponseXml, new FTSingleCreditResponse());
+
+                var RouterResponse = new FundTransferPxResponse
+                {
+                    SourceAccountName = Request.SourceAccountName,
+                    Amount = Request.Amount,
+                    BenefAccountName = Request.BenefAccountName,
+                    BenefAccountNumber = Request.BenefAccountNumber,
+                    BenefKycLevel = Request.BenefKycLevel,
+                    BenefBvn = ResponseObj.BeneficiaryBankVerificationNumber,
+                    BenfBankCode = ResponseObj.DestinationInstitutionCode,
+                    NameEnquiryRef = ResponseObj.NameEnquiryRef,
+                    Narration = ResponseObj.Narration,
+                    SourceAccountNumber = Request.SourceAccountNumber,
+                    TransactionDate = DateTime.Now,
+                    ChannelCode = ResponseObj.ChannelCode,
+                    ResponseCode = ResponseObj.ResponseCode,
+                    ResponseMessage = ResponseObj.ResponseCode == "00" ? "Successful" : null,
+                    SessionId = ResponseObj.SessionID,
+                    TransactionId = Request.TransactionId,
+                    SourceBankCode = Request.SourceBankCode,
+                };
+
+                Log.Write("NibssOutward.Transfer", $"Response to Router: {JsonSerializer.Serialize(RouterResponse)}");
+                return RouterResponse;
+            }
+            catch (Exception Ex)
+            {
+                Log.Write("NibssOutward.Transfer", $"Err: {Ex.Message}");
+                return new FundTransferPxResponse
+                {
+                    ResponseCode = "01",
+                    ResponseMessage = "Failed",
+                    TransactionId = Request.TransactionId,
+                    SourceBankCode = Request.SourceBankCode,
+                };
+            }
         }
 
 
---
+++ NipOutwardProxy/Actions/XmlConverter.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using System.Xml.Serialization;
+ 
+using System.Xml;
+
+namespace NipOutwardProxy.Actions
+{
+    public interface IXmlConverter
+    {
+        object DeSerialize(string XmlString, object Obj);
+        string Serialize(object Obj);
+    }
+
+    public class XmlConverter : IXmlConverter
+    {
+
+
+        public string Serialize(object Obj)
+        {
+            using MemoryStream memoryStream = new MemoryStream();
+            XmlSerializerNamespaces xmlSerializerNamespaces = new XmlSerializerNamespaces();
+            xmlSerializerNamespaces.Add("", "");
+            XmlWriter xmlWriter = XmlWriter.Create(memoryStream);
+            XmlSerializer xmlSerializer = new XmlSerializer(Obj.GetType());
+            xmlSerializer.Serialize(xmlWriter, Obj, xmlSerializerNamespaces);
+            xmlWriter.Flush();
+            memoryStream.Seek(0L, SeekOrigin.Begin);
+            using StreamReader streamReader = new StreamReader(memoryStream, Encoding.UTF8);
+            return streamReader.ReadToEnd();
+        }
+
+        public object DeSerialize(string XmlString, object Obj)
+        {
+            try
+            {
+                XmlDocument xmlDocument = new XmlDocument();
+                xmlDocument.LoadXml(XmlString);
+                XmlSerializer xmlSerializer = new XmlSerializer(Obj.GetType());
+                return xmlSerializer.Deserialize(new StringReader(xmlDocument.InnerXml));
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
