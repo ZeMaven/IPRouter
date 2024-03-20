a7666aad8c57e871ebbc71044eaf88f26a26c447
Peter Akintaro <peter.akintaro@coralpay.com>
2024-03-20 07:50:01 +0000

    Migration

---
+++ MomoSwitch/Models/DataBase/Migrations/20240320074904_migration7.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MomoSwitch.models.database.Migrations
+{
+    /// <inheritdoc />
+    public partial class migration7 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "PortalUserTb",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "PortalUserTb",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "PortalUserTb",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "PortalUserTb");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "PortalUserTb");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "PortalUserTb",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+        }
+    }
+}
