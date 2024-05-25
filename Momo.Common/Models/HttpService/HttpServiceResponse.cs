6c146ef84e28e902ac2074a42e609d5e0eabc68e
Peter Akintaro <peter.akintaro@coralpay.com>
2024-05-25 14:17:39 +0100

    refactoring

---
+++ Momo.Common/Models/HttpService/HttpServiceResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Momo.Common.Models.HttpService
+{
+    public class HttpServiceResponse
+    {
+        public ResponseHeader ResponseHeader { get; set; }
+        public string ResponseContent { get; set; }
+    }
+    public enum Operation { NameEnqury, Transfer, TranQuery }
+}
