4d05456ac06d09e7d9c55e399263abfbf14ae4c1
Peter Akintaro <peter.akintaro@coralpay.com>
2024-07-01 10:30:14 +0100

    Modified Logging with fire and go

--- Momo.Common/Actions/Log.cs
+++ Momo.Common/Actions/Log.cs
@@ -1,13 +1,17 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Diagnostics;
 using System.IO;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 
 namespace Momo.Common.Actions
 {
     public interface ILog
     {
         void Write(string Method, string Message);
+        
     }
 
     public class Log : ILog
@@ -24,24 +28,83 @@ namespace Momo.Common.Actions
 
         }
 
+
         public void Write(string Method, string Message)
         {
             try
             {
-                lock (WriteLock)
+                Task.Run(() =>
                 {
-                    var LogPath = Configuration.GetSection("LogPath").Value;
-                    if (!Directory.Exists(LogPath)) Directory.CreateDirectory(LogPath);
-                    var Writer = new StreamWriter($"{LogPath}/{DateTime.Today.ToString("dd-MMM-yyyy")}.Txt ", true);
-                    Writer.Write($"~ {DateTime.Now.ToString("dd-MMM-yyyy HH:mm:ss fff")} | Method: {Method} | Message: {Message}  {Environment.NewLine}");
-                    Writer.Close();
-                    logger.LogInformation($"~ {DateTime.Now.ToString("dd-MMM-yyyy HH:mm:ss fff")} | Method: {Method} | Message: {Message}");
-                }
+                    lock (WriteLock)
+                    {
+                        var LogPath = Configuration.GetSection("LogPath").Value;
+
+                        if (!Directory.Exists(LogPath)) Directory.CreateDirectory(LogPath);
+                        var Writer = new StreamWriter($"{LogPath}/{DateTime.Today.ToString("dd-MMM-yyyy")}.Txt ", true);
+                        Writer.Write($"~ {DateTime.Now.ToString("dd-MMM-yyyy HH:mm:ss fff")} | Method: {Method} | Message: {Message}  {Environment.NewLine}");
+                        Writer.Close();
+                        logger.LogInformation($"~ {DateTime.Now.ToString("dd-MMM-yyyy HH:mm:ss fff")} | Method: {Method} | Message: {Message}");
+                    }
+                });
             }
             catch
             {
                 logger.LogInformation($"~ {DateTime.Now.ToString("dd-MMM-yyyy HH:mm:ss fff")} | Method: {Method} | Message: {Message}");
             }
         }
+
+        public void Write(string Message, LogType? LogType = null, string Method = null, string Tag = null)
+        {
+            try
+            {
+                Task.Run(() =>
+                {
+                    lock (WriteLock)
+                    {
+                        var LogPath = Configuration.GetSection("LogPath").Value;
+                        if (!Directory.Exists(LogPath)) Directory.CreateDirectory(LogPath);
+                        using (var Writer = new StreamWriter($"{LogPath}/{DateTime.Today:dd-MMM-yyyy}.Txt", true))
+                        {
+                            string MethodName = Method ?? GetCurrentMethodName();
+                            Writer.Write($"~ {DateTime.Now:dd-MMM-yyyy HH:mm:ss fff} | {FormatLogMessage(LogType, MethodName, Message, Tag)}{Environment.NewLine}");
+                        }
+                        logger.LogInformation($"~ {DateTime.Now:dd-MMM-yyyy HH:mm:ss fff} | {FormatLogMessage(LogType, Method ?? GetCurrentMethodName(), Message, Tag)}");
+                    }
+                });
+            }
+            catch
+            {
+                string methodName = Method ?? GetCurrentMethodName();
+                logger.LogInformation($"~ {DateTime.Now:dd-MMM-yyyy HH:mm:ss fff} | {FormatLogMessage(LogType, methodName, Message, Tag)}");
+            }
+        }
+
+        private string GetCurrentMethodName()
+        {
+            var stackTrace = new StackTrace();
+            var methodBase = stackTrace.GetFrame(2).GetMethod(); // Get calling method, 2 frames up in the stack
+            return $"{methodBase.DeclaringType.FullName}.{methodBase.Name}";
+        }
+
+        private string FormatLogMessage(LogType? logType, string method, string message, string tag)
+        {
+            string logTypeStr = logType.HasValue ? $"LogType: {logType} | " : "";
+            string methodStr = !string.IsNullOrEmpty(method) ? $"Method: {method} | " : "";
+            string tagStr = !string.IsNullOrEmpty(tag) ? $"Tag: {tag} | " : "";
+            return $"{logTypeStr}{methodStr}Message: {message}{tagStr}";
+        }
+
+
     }
+
+    public enum LogType
+    {
+        Info,
+        Warning,
+        Request,
+        Response,
+        Tag,
+        Error
+    }
+
 }
